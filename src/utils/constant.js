323e44178da1d80c8d622f754f47cb3206c7585f
Mohd Intakhab Alam <amirint190@gmail.com>
2024-11-24 08:29:41 +0530

    Add files via upload

---
+++ src/utils/constant.js
@@ -0,0 +1,6 @@
+// for running locally
+// export const baseURL = `http://localhost:4000/api/v1`;
+
+// for running on render 
+export const baseURL = `https://todo-backend-irel.onrender.com/api/v1`;
+
